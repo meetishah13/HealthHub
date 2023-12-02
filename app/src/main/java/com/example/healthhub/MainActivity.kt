4cf705fa5758e8eaa82ead93946ddcb32b430295
meetishah <meetishah13@gmail.com>
2023-12-02 16:34:05 -0500

    Splash Screen Done

--- app/src/main/java/com/example/healthhub/MainActivity.kt
+++ app/src/main/java/com/example/healthhub/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Thread.sleep(3000)
-        installSplashScreen()
         setContentView(R.layout.activity_main)
 
     }
